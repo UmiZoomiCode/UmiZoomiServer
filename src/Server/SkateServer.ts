528a4b9aaee1b3383b80ce06a329770b18456427
Reese Bingham <reese.bingham@aggiemail.usu.edu>
2016-10-07 16:40:04 -0600

    Started fleshing out the skate server.

--- src/Server/SkateServer.ts
+++ src/Server/SkateServer.ts
@@ -1,10 +1,31 @@
-import { Controller, ControllerFactory } from "../Controllers"
+import { Controller, ControllerFactory } from "../Controllers";
+import socketIO = require('socket.io')
+import http = require("http");
 
 export class SkateServer {
 	private ActiveController: Controller;
 	private Factory: ControllerFactory;
+	private SocketServer: SocketIO.Server;
+	private Connection;
 	
 	public constructor(factory: ControllerFactory){
 		this.Factory = factory;
+		this.SocketServer = socketIO();
+		
+		this.SocketServer.on('connnection', (socket) => {
+			socket.on('control', (data) => {
+				if(this.ActiveController === undefined){
+					this.Connection = socket;
+					this.ActiveController = this.Factory.createController('socket', {});
+				}
+			});
+			
+			socket.on('disconnect', (data) => {
+				if(socket === this.Connection){
+					this.Connection = undefined;
+					this.ActiveController = undefined;
+				}
+			});
+		});
 	}
 }
